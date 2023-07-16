a6bc86415c5d26a0a0e36af67176613d06e43f42
Joanavel Pascual <joanavelpascual@pursuit.org>
2023-07-16 14:03:51 -0400

    made navbar home link, no styling though

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -4,7 +4,9 @@ import "./NavBar.css";
 function NavBar() {
   return (
     <nav className="navbar">
+      <Link to="/transactions" className="navbar-home-link">
       <h1 className="navbar-title">Joanavel's Budget App</h1>
+      </Link>
       <ul className="navbar-links">
         <li className="navbar-item">
           <Link to="/transactions/new" className="navbar-link">
